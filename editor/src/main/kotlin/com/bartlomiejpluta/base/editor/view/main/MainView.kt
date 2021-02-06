f6fcfdbd81e674333496d1b75716f1ed3e57a881
Bartłomiej Przemysław Pluta <noreply.git@mx.bartlomiejpluta.com>
2021-02-06 11:08:31 +0100

    [Editor] Simplify temporary MainView code

--- editor/src/main/kotlin/com/bartlomiejpluta/base/editor/view/main/MainView.kt
+++ editor/src/main/kotlin/com/bartlomiejpluta/base/editor/view/main/MainView.kt
@@ -1,50 +1,28 @@
 package com.bartlomiejpluta.base.editor.view.main
 
 import com.bartlomiejpluta.base.editor.command.context.UndoableScope
-import com.bartlomiejpluta.base.editor.command.service.UndoRedoService
 import com.bartlomiejpluta.base.editor.controller.map.MapController
-import com.bartlomiejpluta.base.editor.event.RedrawMapRequestEvent
+import com.bartlomiejpluta.base.editor.controller.tileset.TileSetController
 import com.bartlomiejpluta.base.editor.view.map.MapFragment
 import javafx.scene.control.TabPane
+import org.kordamp.ikonli.javafx.FontIcon
 import tornadofx.*
 
 
 class MainView : View() {
-   private val undoRedoService: UndoRedoService by di()
    private val mapController: MapController by di()
+   private val tileSetController: TileSetController by di()
    private val tabPane = TabPane()
 
    override val root = borderpane {
-      top = hbox {
-         button("Map 1") {
+      top = toolbar {
+         button(graphic = FontIcon("fa-file-o")) {
             action {
-               val map = mapController.getMap(1)
+               val tileSet = tileSetController.tileset
+               val map = mapController.createMap(tileSet, 25, 25)
                tabPane += find<MapFragment>(UndoableScope(), MapFragment::map to map).apply {
                   title = "Map 1"
                }
-            }
-         }
-
-         button("Map 2") {
-            action {
-               val map = mapController.getMap(2)
-               tabPane += find<MapFragment>(UndoableScope(), MapFragment::map to map).apply {
-                  title = "Map 2"
-               }
-            }
-         }
-
-         button("Undo") {
-            action {
-               undoRedoService.undo()
-               fire(RedrawMapRequestEvent)
-            }
-         }
-
-         button("Redo") {
-            action {
-               undoRedoService.redo()
-               fire(RedrawMapRequestEvent)
             }
          }
       }
