04d6f6d8984d12d2f1a3960d5ec411a36f2b03fb
Bartłomiej Przemysław Pluta <noreply.git@mx.bartlomiejpluta.com>
2021-02-26 21:53:09 +0100

    [Editor] Refactor ProjectVM

--- editor/src/main/kotlin/com/bartlomiejpluta/base/editor/project/viewmodel/ProjectVM.kt
+++ editor/src/main/kotlin/com/bartlomiejpluta/base/editor/project/viewmodel/ProjectVM.kt
@@ -1,14 +1,19 @@
 package com.bartlomiejpluta.base.editor.project.viewmodel
 
 import com.bartlomiejpluta.base.editor.project.model.Project
-import tornadofx.*
+import tornadofx.ItemViewModel
 
 class ProjectVM(project: Project) : ItemViewModel<Project>(project) {
    val nameProperty = bind(Project::nameProperty)
-   val name by nameProperty
-
    val sourceDirectoryProperty = bind(Project::sourceDirectoryProperty)
-   val sourceDirectory by sourceDirectoryProperty
-
-   val maps = bind(Project::maps)
+   val projectFileProperty = bind(Project::projectFileProperty)
+   val mapsDirectoryProperty = bind(Project::mapsDirectoryProperty)
+   val tileSetsDirectoryProperty = bind(Project::tileSetsDirectoryProperty)
+   val imagesDirectoryProperty = bind(Project::imagesDirectoryProperty)
+   val codeDirectoryProperty = bind(Project::codeDirectoryProperty)
+   val codeFSNodeProperty = bind(Project::codeFSNodeProperty)
+   val buildDirectoryProperty = bind(Project::buildDirectoryProperty)
+   val buildClassesDirectoryProperty = bind(Project::buildClassesDirectoryProperty)
+   val buildOutDirectoryProperty = bind(Project::buildOutDirectoryProperty)
+   val buildOutputJarFileProperty = bind(Project::buildOutputJarFileProperty)
 }
