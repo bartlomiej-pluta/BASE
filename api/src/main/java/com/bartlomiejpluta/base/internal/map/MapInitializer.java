fc96de47c55486800bf34116a99aad41b5da46bb
Bartłomiej Przemysław Pluta <noreply.git@mx.bartlomiejpluta.com>
2021-04-03 12:22:16 +0200

    Create MapInitializer internal interface

---
+++ api/src/main/java/com/bartlomiejpluta/base/internal/map/MapInitializer.java
@@ -0,0 +1,9 @@
+package com.bartlomiejpluta.base.internal.map;
+
+import com.bartlomiejpluta.base.api.context.Context;
+import com.bartlomiejpluta.base.api.map.handler.MapHandler;
+import com.bartlomiejpluta.base.api.runner.GameRunner;
+
+public interface MapInitializer {
+   void run(final Context context, final GameRunner runner, final MapHandler handler);
+}
